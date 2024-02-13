6f924c74086aadd099d775d6baa2654a01e0873f
Jayden883 <radulajoshua@gmail.com>
2024-02-13 17:43:52 +0300

    vercel a

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -19,12 +19,7 @@ export default function Header() {
           <div className="border-l border-gray-300"></div>
           <div>Any week</div>
           <div className="border-l border-gray-300"></div>
-          <div>Add guests</div>
-          <button className='bg-primary text-white p-1 rounded-full'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-            </svg>
-          </button>
+          <div>Any guest</div>
         </div>
         <Link to={user?'/account':'/login'} className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
