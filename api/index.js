caa7be31afa374230763bde26fbee7e1f90c1381
Jayden883 <radulajoshua@gmail.com>
2024-02-14 05:23:12 +0300

    api3

--- api/index.js
+++ api/index.js
@@ -93,7 +93,7 @@ app.post('/api/login', async (req, res) => {
     }
 })
 
-app.get('/profile', (req, res) => {
+app.get('/api/profile', (req, res) => {
     mongoose.connect(process.env.MONGO_URL);
     const {token} = req.cookies;
     if (token) {
